3c1ba84e85c41043eaa268cc3350002b68a89de6
trantienvn <97656011+trantienvn@users.noreply.github.com>
2024-11-30 19:15:34 +0700

    Update Locket_trantien.js

--- js/Locket_trantien.js
+++ js/Locket_trantien.js
@@ -1,36 +1,52 @@
-// ========= ID ========= //
 const mapping = {
-    '%E8%BD%A6%E7%A5%A8%E7%A5%A8': ['vip+watch_vip'],
-    'Locket': ['Gold']
+  '%E8%BD%A6%E7%A5%A8%E7%A5%A8': ['vip+watch_vip'],
+  'Locket': ['Gold']
 };
-// =========   Phần cố định  ========= // 
-// =========  @trantien ========= // 
-var ua = $request.headers["User-Agent"] || $request.headers["user-agent"],
-    obj = JSON.parse($response.body);
+
+var ua = $request.headers["User-Agent"] || $request.headers["user-agent"];
+var obj = JSON.parse($response.body);
+
+// Thêm thông báo chú ý
 obj.Attention = "Chúc mừng bạn! Vui lòng không bán hoặc chia sẻ cho người khác!";
-var trantien = {
-        is_sandbox: !1,
-        ownership_type: "PURCHASED",
-        billing_issues_detected_at: null,
-        period_type: "normal",
-        expires_date: "2099-12-18T01:04:17Z",
-        grace_period_expires_date: null,
-        unsubscribe_detected_at: null,
-        original_purchase_date: "2024-07-28T01:04:18Z",
-        purchase_date: "2024-07-28T01:04:17Z",
-        store: "app_store"
-    },
-    tien = {
-        grace_period_expires_date: null,
-        purchase_date: "2024-07-28T01:04:17Z",
-        product_identifier: "com.trantien.premium.yearly",
-        expires_date: "2099-12-18T01:04:17Z"
-    };
-const match = Object.keys(mapping).find(e => ua.includes(e));
+
+// Cấu hình thông tin đăng ký
+var locket02 = {
+  is_sandbox: false,
+  ownership_type: "PURCHASED",
+  billing_issues_detected_at: null,
+  period_type: "normal",
+  expires_date: "2099-12-18T01:04:17Z",
+  grace_period_expires_date: null,
+  unsubscribe_detected_at: null,
+  original_purchase_date: "2024-04-12T01:04:18Z",
+  purchase_date: "2024-04-12T01:04:17Z",
+  store: "app_store"
+};
+
+var locket01 = {
+  grace_period_expires_date: null,
+  purchase_date: "2024-04-12T01:04:17Z",
+  product_identifier: "com.locket02.premium.yearly",
+  expires_date: "2099-12-18T01:04:17Z"
+};
+
+// Kiểm tra và ánh xạ User-Agent
+const match = Object.keys(mapping).find(key => ua.includes(key));
+
 if (match) {
-    let [e, s] = mapping[match];
-    s ? (tien.product_identifier = s, obj.subscriber.subscriptions[s] = trantien) : obj.subscriber.subscriptions["com.trantien.premium.yearly"] = trantien, obj.subscriber.entitlements[e] = tien
-} else obj.subscriber.subscriptions["com.trantien.premium.yearly"] = trantien, obj.subscriber.entitlements.pro = tien;
-$done({
-    body: JSON.stringify(obj)
-});
+  let [entitlement, subscription] = mapping[match];
+
+  if (subscription) {
+    locket01.product_identifier = subscription;
+    obj.subscriber.subscriptions[subscription] = locket02;
+  } else {
+    obj.subscriber.subscriptions["com.locket02.premium.yearly"] = locket02;
+  }
+
+  obj.subscriber.entitlements[entitlement] = locket01;
+} else {
+  obj.subscriber.subscriptions["com.locket02.premium.yearly"] = locket02;
+  obj.subscriber.entitlements.pro = locket01;
+}
+
+$done({ body: JSON.stringify(obj) });
